e694a7afa71e11fd5eeeca85761ca5f3edc02a2a
Evgenysmo <Evgenysm@GLB-DG-05.trad.local>
2016-03-06 19:10:35 +0200

    update

--- app/controllers/signupCtrl.js
+++ app/controllers/signupCtrl.js
@@ -5,6 +5,7 @@ templateCtrls.controller('signupCtrl', ['$scope','app.factories.data', function 
     $scope.save = function(isValid){
         if(isValid){
             console.log($scope.user);
+            data.addUser($scope.user);
         }
         else{
             console.log('not valid');
